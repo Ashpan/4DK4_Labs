428ef92d9dd5bdeeeb8433de94ed465383108f3a
Ashpan Raskar <ashpanr@gmail.com>
2022-11-06 13:28:04 -0500

    question 2 complete

---
+++ Lab4/simparameters.h
@@ -0,0 +1,49 @@
+
+/*
+ * Simulation of the ALOHA Protocol
+ *
+ * Copyright (C) 2014 Terence D. Todd Hamilton, Ontario, CANADA
+ * todd@mcmaster.ca
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*******************************************************************************/
+
+#ifndef _SIMPARAMETERS_H_
+#define _SIMPARAMETERS_H_
+
+/*******************************************************************************/
+
+#define NUMBER_OF_STATIONS 5
+#define MEAN_PACKET_DURATION 1      /* normalized packet Tx time */
+#define PACKET_ARRIVAL_RATE 0.1     /* packets per Tx time */
+#define MEAN_BACKOFF_DURATION 7    /* in units of packet transmit time, Tx */
+#define RUNLENGTH 70000 //70000000
+#define BLIPRATE 100000
+
+/* Comma separated list of random seeds to run. */
+#define RANDOM_SEED_LIST 400185326
+#define ARRIVAL_RATE_LIST 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0   /* packets per Tx time */
+
+/*******************************************************************************/
+
+#endif /* simparameters.h */
+
+
+
+
+
+
