c78894ab96dcf82a4113422c8387f72d929b9802
Dharak Verma <vermad1@mcmaster.ca>
2022-11-13 11:55:50 -0500

    Add lab5 WIP code

---
+++ coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/channel.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/channel.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/cleanup.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/cleanup.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/main.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/main.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/output.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/output.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/packet_arrival.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/packet_arrival.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/packet_duration.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/packet_duration.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/packet_transmission.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/packet_transmission.c.o"
+  "/media/psf/Home/Documents/coe4dk4_lab_4_2022/simlib.c" "/media/psf/Home/Documents/coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/simlib.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ coe4dk4_lab_4_2022/build/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/channel.c.o"
+  "CMakeFiles/run.dir/cleanup.c.o"
+  "CMakeFiles/run.dir/main.c.o"
+  "CMakeFiles/run.dir/output.c.o"
+  "CMakeFiles/run.dir/packet_arrival.c.o"
+  "CMakeFiles/run.dir/packet_duration.c.o"
+  "CMakeFiles/run.dir/packet_transmission.c.o"
+  "CMakeFiles/run.dir/simlib.c.o"
+  "run"
+  "run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
